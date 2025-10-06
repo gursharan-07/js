496d9b4e3af7fe5a494c3d09d79cf75e5ca4ac21
gursharan-07 <muschab45@gmail.com>
2025-10-06 05:56:45 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accid=12345
+let accmail="gmkaur"
+var accp="123"
+accity="jaipur"
+accmail="muschab"
+accp="7777"
+accity="blr"
+let accstate;
+console.table([accid,accmail,accp,accity,accstate])
+console.log(typeof undefined)
+console.log(typeof null)
